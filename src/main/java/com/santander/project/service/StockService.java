baa9a9a93194894b1c075214ad1c41f942076062
lucas.caminha <lucas_santoscaminha@hotmail.com>
2021-05-27 18:52:52 -0300

    Adicionado Critica de Stock repetido e ExceptionHandler

--- src/main/java/com/santander/project/service/StockService.java
+++ src/main/java/com/santander/project/service/StockService.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.santander.project.exceptions.BusinessException;
 import com.santander.project.mapper.StockMapper;
 import com.santander.project.model.Stock;
 import com.santander.project.model.dto.StockDTO;
 import com.santander.project.repository.StockRepository;
+import com.santander.project.util.MessageUtils;
 
 @Service
 public class StockService {
@@ -25,7 +27,9 @@ public class StockService {
 	@Transactional
 	public StockDTO save(@Valid StockDTO dto) {
 		Optional<Stock> optionalStock = stockRepo.findByNameAndDate(dto.getName(), dto.getDate());
-		
+		if(optionalStock.isPresent()) {
+			throw new BusinessException(MessageUtils.STOCK_ALREADY_EXISTS);
+		}
 		Stock stock = stockMapper.toEntity(dto);
 		stockRepo.save(stock);
 		return stockMapper.toDto(stock);
---
+++ src/main/java/com/santander/project/util/MessageUtils.java
@@ -0,0 +1,7 @@
+package com.santander.project.util;
+
+public abstract class MessageUtils {
+
+	public static final String STOCK_ALREADY_EXISTS = "Stock already exists in the database";
+	
+}
